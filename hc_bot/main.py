4dd85dd30bc50e247e095e3ca437a03093e3d787
Samuil Petrov <samuil.p@pigeon-line.com>
2017-11-18 02:04:59 +0100

    Testing API

--- hc_bot/main.py
+++ hc_bot/main.py
@@ -8,8 +8,10 @@ Example: $ python main.py
 
 Author: Samuil Petrov
 """
-
+import tweepy
 import pandas as pd
+from credentials import *
+from time import sleep
 
 def get_channels(posts):
     """Decide whether post is for Twitter or for Facebook"""
@@ -18,6 +20,14 @@ def get_channels(posts):
             yield "twitter" 
         else:
             yield "facebook"
+
+# Authorization
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+tweet = "Hello, world!"
+api.update_status(status=tweet)
 
 posts = pd.read_excel('../Python_exercise_GT_Linkers.xlsx', sheet_name='data_table')
 
